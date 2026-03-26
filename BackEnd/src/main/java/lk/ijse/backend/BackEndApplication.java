4a9b6a6f02ff1a8b1c52df65e56c54d9a886cab9
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2026-03-26 22:49:23 +0530

    fix pdf and algorithm scoring logic

--- BackEnd/src/main/java/lk/ijse/backend/BackEndApplication.java
+++ BackEnd/src/main/java/lk/ijse/backend/BackEndApplication.java
@@ -2,8 +2,10 @@ package lk.ijse.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class BackEndApplication {
 
     public static void main(String[] args) {
